e4f781520071311833d7e54fb9d82e9a0d186973
Rajeev Ranjan Prasad <rajeevranjan125@users.noreply.github.com>
2026-07-06 22:52:13 +0530

    added object

--- react-learning/src/Hello.tsx
+++ react-learning/src/Hello.tsx
@@ -1,10 +1,16 @@
 function Hello() {
 
     const arr = ["hello", "world", "hmmm"];
+    const obj = {
+        id:1,
+        name:"rajeev"
+    }
 
     return (
         <div>
             <h1>{arr[0] + " " + arr[1] + " " + arr[2]}</h1>
+            <h2>{obj.id}</h2>
+            <h2>{obj.name}</h2>
         </div>
     )
 }
