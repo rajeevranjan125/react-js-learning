d6608be0e290dd2461fab2e66e6dee8f22977c95
Rajeev Ranjan Prasad <rajeevranjan125@users.noreply.github.com>
2026-07-06 22:54:23 +0530

    added h2

--- react-learning/src/Hello.tsx
+++ react-learning/src/Hello.tsx
@@ -9,7 +9,9 @@ function Hello() {
     return (
         <div>
             <h1>{arr[0] + " " + arr[1] + " " + arr[2]}</h1>
+        
             <h2>{obj.id}</h2>
+        
             <h2>{obj.name}</h2>
         </div>
     )
